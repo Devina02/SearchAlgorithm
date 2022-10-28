45325c81e04d8c5fc0532344ff0249c2b39c2b78
Devina02 <devina.fitriapngs@gmail.com>
2022-10-28 16:03:43 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchAlgorithm
+{
+    class Program
+    {
+        //Array to be searched
+        int[] arr = new int[20];
+        //Number of elements in the array
+        int n;
+        //get the number of element the store in the array
+        int i;
+
+        public void input()
+        {
+            while (true)
+            {
+                Console.Write(" Enter the number of elements in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if ((n > 0) && (n <= 20))
+                    break;
+                else
+                    Console.WriteLine()
+
+            }
+        }
+        
+    }
+}
